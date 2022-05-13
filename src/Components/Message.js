5b3b6e13179245ca5fd8c5d54013192f8fd0262f
Domagoj Zganec <domagoj.zganec1@gmail.com>
2022-05-13 21:15:15 +0200

    refactored

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,77 +1,22 @@
-import { useState } from "react"
-import MessageBubble from "./MessageBubble"
+export default function MessageBubble({ message, replyCount }) {
+  const isChild = Boolean(message.parent_id);
+  const msgClass = isChild ? "msg--img-body-child" : "msg--img-body";
 
-export default function Message(props) {
-    const [replyCount, setReplyCount] = useState(0)
-    const timestamp = props.properties.timestamp
-    const dayNum = new Date(timestamp).getDay()
-    const date = new Date(timestamp).toLocaleString(
-        'en-US', { hour: 'numeric', minute: 'numeric', hour12: true }
-    )
-
-    let day = ""
-    switch(dayNum) {
-        case 0: {
-            day = "Sunday"
-            break;
-        }
-        case 1:{ 
-            day = "Monday"
-            break;
-        }
-        case 2:{ 
-            day = "Tuesday"
-            break;
-        }
-        case 3:{ 
-            day = "Wednesday"
-            break;
-        }
-        case 4:{ 
-            day = "Thursday"
-            break;
-        }
-        case 5:{ 
-            day = "Friday"
-            break;
-        }
-        case 6:{ 
-            day = "Saturday"
-            break;
-        }
-        default:{
-            day = "n/a"
-            break;
-        }
-    }
-
-    const localDate = new Date(timestamp).toLocaleString("HR-hr", { day: "numeric", month: "numeric", year: "numeric" })
-    const fullDate = localDate.replace(/ /g, "")
-
-    
-    const childMessages = props.children.map(element => {
-        return <MessageBubble
-            properties={{
-                author: element.author,
-                id: element.id,
-                parent_id: element.parent_id,
-                text: element.text,
-                timestamp: element.timestamp
-            }}
-            date={date}
-            setReplyCount={setReplyCount}
-        />
-    })
-
-    return (
-        <div className="msg">
-            {!props.properties.parent_id && <p className="msg--date">{`${day}, ${fullDate}`}</p>}
-            <MessageBubble
-                properties={props.properties}
-                date={date}
-                replyCount={replyCount}
-            />
-            {childMessages}
+  return (
+    <div className={msgClass}>
+      <img className="msg--img" alt="img" src={message.author.picture} />
+      <div className="msg--body">
+        <div className="msg--bubble">
+          <h4>{message.author.name}</h4>
+          <p>{message.text}</p>
         </div>
-    )
+        <span className="msg--time-sent">
+          {message.date} |{" "}
+          <span className="msg--reply">
+            Reply {!isChild && replyCount > 0 && `(${replyCount})`}
+          </span>
+        </span>
+      </div>
+    </div>
+  );
 }
